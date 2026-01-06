b7d7edf1f9623aa4ddd89a7e43785c3be8f2596b
ryanperna <ryannicholasp@nycstudents.net>
2026-01-06 17:57:46 +0000

    i hate this class

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function showStatue() {
+  document.getElementById("highlight").innerHTML = "Statue of Liberty";
+}
+
+function showEmpire() {
+  document.getElementById("highlight").innerHTML = "Empire State Building";
+}
+
+function showPark() {
+  document.getElementById("highlight").innerHTML = "Central Park";
+}
+
+function showBridge() {
+  document.getElementById("highlight").innerHTML = "Brooklyn Bridge";
+}
+
+function showTimesSquare() {
+  document.getElementById("highlight").innerHTML = "Times Square";
+}
