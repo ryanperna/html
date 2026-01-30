8b39d6acfb463f13b0e66e2887cdaf27150d8b7a
ryanperna <ryannicholasp@nycstudents.net>
2026-01-30 18:14:57 +0000

    qweqweqweqweqw

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -3,19 +3,24 @@
 
 
 /* Challenge 4: Does the function name match the event handler referenced in the event listener? */
-function volume(){
+ function volume(){
+  
 
   /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  /* line 11 "getElementById" This function does not exist in JavaScript. * line 13 By in get element by it isnt capital*/
+  let r = parseInt(document.getElementById("r").value);
+  let height = parseInt(document.getElementById("height").value);
 
   /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  /* line 16 output is spelled with a capital o in the html script this is not supposted to have a .value*/
+  let output = document.getElementById("Output");
 
   /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  /* height is referred to as "h" instead of height and "math" should be spelled with a capital M formula is times height not divided by height */
+
+  let v = Math.PI * Math.pow(r,2) * height;
 
   /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
-
+  /* line 23 not closed with a ` */
+  output.innerHTML = `Volume of the cylinder is ${v}`
 }
