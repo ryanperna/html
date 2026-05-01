659a8658a26af374295b54eb9e5e99f4bb3fedbb
ryanperna <ryannicholasp@nycstudents.net>
2026-05-01 17:20:04 +0000

    fghfghfg

--- WDLesson6_7Pratical/script.js
+++ WDLesson6_7Pratical/script.js
@@ -23,8 +23,15 @@ async function init(){
               </div>`    
   }
   output.innerHTML = build;
-}
 
+
+let vtypes = fillDropDown("vehicle_type_code1");
+  document.getElementById("vtypes").innerHTML = vtypes;
+
+
+
+
+}
 
 
 function SearchByID(){
@@ -58,7 +65,7 @@ function SearchByID(){
 
 function SearchByStreet(){
   let output = document.getElementById("output");
-  let off_street_name = document.getElementById("Off").value;
+  let on_street_name = document.getElementById("Off").value;
   let result = document.getElementById("result");
   
   let build = "";
@@ -66,7 +73,7 @@ function SearchByStreet(){
 
   for(let i = 0; i < data.length; i+=1){
     let M = data[i];
-    if(M.off_street_name == off_street_name){
+    if(M.on_street_name == on_street_name){
      build += `<div class="fitted card">
                  <h3>${M.on_street_name}</h3>
                  <hr>
@@ -85,26 +92,37 @@ function SearchByStreet(){
   output.innerHTML = build;
 }
 
-function fillDropDown(key){
-  let list = [];
-  let build = `<option>Select Option</option>`
-  for(let i = 0; i < data.length; i++){
-    let data_field = data[i];
-    if(!list.includes(data_field[key])){
-      list.push(data_field[key]);
+function SearchByFactor(){
+  let output = document.getElementById("output");
+  let contributing_factor_vehicle_1 = document.getElementById("factor").value;
+  let result = document.getElementById("result");
+  
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let M = data[i];
+    if(M.contributing_factor_vehicle_1 == contributing_factor_vehicle_1){
+     build += `<div class="fitted card">
+                 <h3>${M.on_street_name}</h3>
+                 <hr>
+                 <p>${M.off_street_name}</p>
+                 <p>${M.crash_date}</p>
+                 <p>${M.contributing_factor_vehicle_1}</p>
+                 <hr>
+                 <p>Vehicle type- ${M.vehicle_type_code1}</p>
+                 <hr>
+                 <p>ID ${M.collision_id}</p>
+              </div>`    
+      ct += 1;
     }
   }
-  list.sort();
-  for(let field of list){
-      build += `<option>${field}</option>`;
-  }
-  return build;
+  result.innerHTML = `${ct} Results found.`
+  output.innerHTML = build;
 }
 function filterbyvtype(){
-  let vtypes = fillDropDown("contributing_factor_vehicle_1");
-  document.getElementById("vtype").innerHTML = d4eefs;
   let output = document.getElementById("output");
-  let contributing_factor_vehicle_1 = document.getElementById("vtpye").value;
+  let contributing_factor_vehicle_1 = document.getElementById("vtype").value;
   let result = document.getElementById("result");
   
   let build = "";
@@ -129,4 +147,6 @@ function filterbyvtype(){
   }
    result.innerHTML = `${ct} Results found.`
    output.innerHTML = build;
-}
+
+  }
+
