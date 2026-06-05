ca657349a323d4a95d7ab88dca4da0992f9e549e
ryanperna <ryannicholasp@nycstudents.net>
2026-06-05 17:16:44 +0000

    dfgdfgdg

--- Project final /helper.js
+++ Project final /helper.js
@@ -3,25 +3,18 @@ function get(id){
   return document.getElementById(id);
 }
 
-// Challenge 3: Create the function showMap() that displays the map for a location [lat, lon]
-function showMap(lat,lon){
-  
+// showMap() displays the map for a location array [lat, lon]
+function showMap(location){ 
+  if(!mapObj){
+      mapObj = L.map("map");
+  }
 
+  let map = mapObj.setView(location, 14);
 
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
 
-
-}
-
-// Challenge 4: Create the function card() to generate an appropriate info card with the button to view map
-function card( info ){ 
-  let build = `<div class="card fitted">
-                  <h3>${info.crash_date}</h3>
-                  <hr>
-                  <p>Location: ${info.crash_time}</p>
-                  <h5>${info.borough}</h5>`;
-                  if(info.latitude && info.longitude){
-                    build += `<input type="button" value="Map" onclick="showMap( ${info.latitude}, ${info.longitude} )">`;
-                  }
-    build +=    `</div>`;
-  return build;
+  let marker = L.marker(location).addTo(map);
 }
